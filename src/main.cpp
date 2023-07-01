90453540e2a9a8d19c36dcae7f734ca41d9b1916
Joe Fowler <scrumplesplunge@gmail.com>
2023-07-01 14:22:23 +0100

    Load and compile shaders.

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,51 @@
 #include <GLFW/glfw3.h>
 #include <glad/gl.h>
 
+#include <array>
+#include <filesystem>
+#include <fstream>
 #include <iostream>
+#include <span>
 #include <string_view>
 
 [[noreturn]] void Die(std::string_view reason) {
   std::cerr << "Fatal error: " << reason << '\n';
   std::exit(1);
+}
+
+std::string GetContents(const std::filesystem::path& path) {
+  std::ifstream file(path, std::ios::binary);
+  std::string contents{std::istreambuf_iterator<char>(file), {}};
+  if (!file.good()) Die("GetContents");
+  return contents;
+}
+
+GLuint LoadShader(GLuint type, const std::filesystem::path& path) {
+  const std::string code = GetContents(path);
+  GLuint shader = glCreateShader(type);
+  const GLchar* const source = code.data();
+  const GLint source_length = code.size();
+  glShaderSource(shader, 1, &source, &source_length);
+  glCompileShader(shader);
+
+  GLint success;
+  glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+  if (!success) Die("glCompileShader");
+  return shader;
+}
+
+GLuint LinkProgram(std::span<const GLuint> shaders) {
+  GLuint program = glCreateProgram();
+  for (GLuint shader : shaders) glAttachShader(program, shader);
+  glLinkProgram(program);
+
+  GLint linked, validated;
+  glGetProgramiv(program, GL_LINK_STATUS, &linked);
+  if (!linked) Die("glLinkProgram");
+  glValidateProgram(program);
+  glGetProgramiv(program, GL_VALIDATE_STATUS, &validated);
+  if (!validated) Die("glValidateProgram");
+  return program;
 }
 
 int main() {
@@ -20,6 +59,14 @@ int main() {
   GLuint vertex_array;
   glGenVertexArrays(1, &vertex_array);
   glBindVertexArray(vertex_array);
+
+  const GLuint vertex_shader = LoadShader(GL_VERTEX_SHADER, "src/shader.vert");
+  const GLuint fragment_shader =
+      LoadShader(GL_FRAGMENT_SHADER, "src/shader.frag");
+  const GLuint program =
+      LinkProgram(std::array{vertex_shader, fragment_shader});
+  glDeleteShader(vertex_shader);
+  glDeleteShader(fragment_shader);
 
   while (!glfwWindowShouldClose(window)) {
     glfwPollEvents();
