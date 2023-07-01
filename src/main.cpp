093e7a2c82d219751a6c497fc6164db8c1a50bcb
Joe Fowler <scrumplesplunge@gmail.com>
2023-07-01 13:35:49 +0100

    Check for errors.

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,17 @@
 #include <GLFW/glfw3.h>
 
 #include <iostream>
+#include <string_view>
+
+[[noreturn]] void Die(std::string_view reason) {
+  std::cerr << "Fatal error: " << reason << '\n';
+  std::exit(1);
+}
 
 int main() {
-  glfwInit();
+  if (!glfwInit()) Die("glfwInit");
   GLFWwindow* window = glfwCreateWindow(640, 480, "Game", nullptr, nullptr);
+  if (!window) Die("glfwCreateWindow");
 
   while (!glfwWindowShouldClose(window)) {
     glfwWaitEvents();
