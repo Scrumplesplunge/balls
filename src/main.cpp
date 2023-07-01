c77fe9ae76f145c42d99824fee1a55ccdd6fd451
Joe Fowler <scrumplesplunge@gmail.com>
2023-07-01 13:33:46 +0100

    Create a GLFW window.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,13 @@
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+
 #include <iostream>
 
 int main() {
-  std::cout << "Hello, World!\n";
+  glfwInit();
+  GLFWwindow* window = glfwCreateWindow(640, 480, "Game", nullptr, nullptr);
+
+  while (!glfwWindowShouldClose(window)) {
+    glfwWaitEvents();
+  }
 }
